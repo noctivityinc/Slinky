007a01e87466900ebfc1518b2fc70803701c5df1
Noctivity Inc <jlippiner@noctivity.com>
2011-11-03 15:31:03 -0400

    added fail display message support

--- js/slinky.js
+++ js/slinky.js
@@ -27,12 +27,29 @@ function Slinky(){
 			return this.run();
 		},
 		
-		assert: function( outcome, explaination ) {
+		assert: function( outcome, explaination, failDisplay) {
         outcome ? passed++ : failed++;
 
         var li = document.createElement('li');
         li.className = outcome ? 'pass' : 'fail';
         li.appendChild( document.createTextNode( explaination ) );
+
+				if(!outcome && failDisplay!==undefined) {
+					var ul = document.createElement('ul');
+					if(typeof failDisplay == 'object') {
+						for(i=0;i<failDisplay.length;i++) {
+			        var li2 = document.createElement('li');
+			        li2.appendChild( document.createTextNode( failDisplay[i] ) );
+							ul.appendChild(li2);
+						}
+					} else {
+		        var li2 = document.createElement('li');
+		        li2.appendChild( document.createTextNode( failDisplay ) );
+						ul.appendChild(li2);
+					}
+					li.appendChild(ul);
+				}
+
         output.appendChild(li);
     },
 
